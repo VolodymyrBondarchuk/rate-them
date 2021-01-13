f9769e34ef66c8b52255e981f058d821b53806f8
Volodymyr Bondarchuk <w.volodymyr.bondarchuk@gmail.com>
2021-01-13 17:48:43 +0200

    updated backend url

--- src/api/const/backend-url.js
+++ src/api/const/backend-url.js
@@ -1,6 +1,6 @@
 export let config = {
   git: "https://api.github.com",
   repositories: "/repositories",
-  backendDev: "http://localhost:8080",
+  backendDev: "http://ec2-52-15-92-138.us-east-2.compute.amazonaws.com:8080",
   addReviewUrl: "/api/review/add"
 }
